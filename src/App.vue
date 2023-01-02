fe5dca80c58c9b56e3809430e2e89181885bda12
Ivan Kulagin <ivan.kulagin2001@gmail.com>
2023-01-03 01:02:29 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      Hello World {{ counter }}
+    </h1>
+    <button @click="increaseCounter">Increase</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path")
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.esm.js"
+        },
+        extensions: ["*", ".js", ".vue", ".json"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new VueLoaderPlugin()
+    ]
+}
